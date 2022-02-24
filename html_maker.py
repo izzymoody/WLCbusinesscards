a4e388a4ccb1e76d53047f0003f918ab1ef0ea6b
Izzy Moody <78048580+izzymoody@users.noreply.github.com>
2022-02-24 13:57:17 -0500

    Add files via upload

--- html_maker.py
+++ html_maker.py
@@ -2,50 +2,94 @@ import csv
 
 def html_maker(wildcatsync_fourm):
     
-    #print()
-
-    #with open(wildcatsync_fourm) as csvfile:
-        #data = list(csv.reader(csvfile))
-        #data = data[2:]
-        #print(data)
-       # col_names = {}
-        #col_indexes = {}
-        #headers = data[2]
-        #headers = []
-        #counter = 0
-        #for i in range(len(data[0])):
-           # col_names[data[0][i]]=i
-           # col_indexes[i]= data[0][i]
+    
     data = open(wildcatsync_fourm, 'r')
-
-    lines = data.readlines()
-  
-
-    this_line = lines[4] #string for person 
-    this_line = this_line.split(',') #split makes it an array 
-    #print(this_line[3]) # indexing now selects field from fourm 
+    lines = data.readlines() 
+    #lname = "moody"
+    #output_name = "/Users/izzymoody/Desktop/WLC/digital-namecard-master/%s.csv" %lname
    
     #i was thinking if we manually write the indexes of the info we need then select 
     # it in the for loop ex email = 3 so we can say this_line[email]
-    email = 3
+    email = 17
+    fname = 4
+    lname = 5
+    cell = 18 # need to add dashes for some people 
+    pronouns = 16
+    major = 33
+    website = 33
+    linkedin = 34
+    d_connect = 35
+    social = 37
+    quote = 38
+
     #this for loop works 
     for line in range(len(lines)):
-        this_line = lines[line].split(',')
+        this_line = lines[line].split('\t')
         for field in range(len(this_line)):
-            if line >= 3:
+            if line >= 4:
                 if field == email:
                     print(this_line[email])
-            
-    
+                    email_field(str(this_line[email]))
+               # print(this_line[field],"\t", field)
+                
 
+                
+            
     return 0 
 
-def field_maker(parameter):
+def header():
+    """
+    <!DOCTYPE html>
+    <html>
 
+    <head>
+    <link rel="stylesheet" href="main.css">
+    <link href='https://fonts.googleapis.com/css?family=Lato:200,300,400,700' rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/css?family=Muli:200,300" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    </head>
+
+    <body>
+	<section class="bg-color-grey banner">
+		<div class="container">
+			<div class="row">
+				<div class="col"></div>
+				<div class="col is-5">
+					<img class="profile-pic" src="logo.png" alt="Profile Picture">
+				</div>
+				<div class="col"></div>
+			</div>
+		</div>
+	</section>
+    
+    """
+def email_field(parameter):
+    section = """
+    <section class="bg-color-white secondary-info">
+		<a href="mailto:"{}">
+			<div class="container">
+				<div class="row detail">
+					<div class="col">
+						<img src="assets/icon-email" alt="Email Icon"">
+					</div>
+					<div class="col is-8">
+						<p class="text-detail">{}</p>
+					</div>
+					<div class="col">
+						<img src="assets/right-arrow.svg" alt="Right Arrow" height="50%" width="40%">
+					</div>
+				</div>
+			</div>
+		</a>
+	</section>""".format(parameter, parameter)
+
+    print(section)
+    
     return 0 
 
 def main():
-    filename = "FormSubmissions.csv"
+    filename = "/Users/izzymoody/Desktop/WLC/digital-namecard-master/FormSubmissions.txt"
+   
     html_maker(filename)
     return 0
 
